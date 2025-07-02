939dc85a88108ee323e6b814f68dfa07fd8a552c
Ruojing Li <47389704+TinaLRJ@users.noreply.github.com>
2025-07-02 12:00:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -116,7 +116,7 @@ def main(args):
     print("start loading test data ...")
     TEST_DATASET  = TestIRSeqDataLoader(args.dataset, data_root=root,  seq_len=SEQ_LEN, cat_len=int(SEQ_LEN*0.1), transform=None)
 
-    trainDataLoader = torch.utils.data.DataLoader(TRAIN_DATASET, batch_size=BATCH_SIZE, shuffle=True, num_workers=4,
+    trainDataLoader = torch.utils.data.DataLoader(TRAIN_DATASET, batch_size=BATCH_SIZE, num_workers=4,
                                                   pin_memory=True, drop_last=True, sampler=sampler,
                                                   worker_init_fn=lambda x: np.random.seed(x + int(time.time())))
 
